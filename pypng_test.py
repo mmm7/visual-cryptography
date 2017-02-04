cba8a45534211ab4d1ae633b10a930655de3da18
SZALAY Mate <mate.szalay@gmail.com>
2017-02-04 22:20:45 +0100

    png writing

--- pypng_test.py
+++ pypng_test.py
@@ -1,8 +1,10 @@
 import unittest2 as unittest
 
 import png
+import tempfile
 
 class PyPngTest(unittest.TestCase):
+  DIR = tempfile.mkdtemp()
   def test01(self):
     i = png.Reader(filename='testdata/test01.png').asDirect()
     self.assertEquals(32, i[0])
@@ -16,3 +18,16 @@ class PyPngTest(unittest.TestCase):
     self.assertEquals([255, 255, 0], list(d[0]))
     self.assertEquals([0, 0, 0], list(d[1]))
 
+  def testWrite(self):
+    fname = self.DIR + '/ttt.png'
+    s = [[0,1,0,1],[1,1,0,0]]
+    with open(fname, 'wb') as f:
+      w = png.Writer(len(s[0]), len(s), greyscale=True, bitdepth=1)
+      w.write(f, s)
+
+    i = png.Reader(filename=fname).asDirect()
+    self.assertEquals(4, i[0])
+    self.assertEquals(2, i[1])
+    d = list(i[2])
+    self.assertEquals([0, 1, 0, 1], list(d[0]))
+    self.assertEquals([1, 1, 0, 0], list(d[1]))
