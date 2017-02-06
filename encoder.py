621488a650a4cfbeb4766549b73f1b651761c70c
SZALAY Mate <mate.szalay@gmail.com>
2017-02-06 22:24:41 +0100

    encoder + tests

---
+++ encoder.py
@@ -0,0 +1,45 @@
+def get_params(masks):
+  """ Return the important parameters of a mask: (n, pixel_x, pixel_y).
+
+  n: Number of output images (different masks for a pixel).
+  pixel_x: width of a single masK
+  pixel_y: height of a single mask
+  """
+  black_masks, white_masks = masks
+  sample_mask = white_masks[0][0]
+  n = len(white_masks[0])
+  pixel_y = len(sample_mask)
+  pixel_x = len(sample_mask[0])
+  return n, pixel_x, pixel_y
+
+def apply_masks(image, masks, random):
+  """Apply the visual cryptography masks from 'masks' to 'image'.
+
+  A list of output images is returned.
+  The number and size of the output images is determined by 'masks'.
+  'random' is the random number source. random.Random() can be passed.
+  """
+  n, pixel_x, pixel_y = get_params(masks)
+  black_masks, white_masks = masks
+  # outputs is a list of output images.
+  outputs = [[] for _ in xrange(n)]
+  assert n == len(outputs)
+  for row in image:
+    assert n == len(outputs)
+    # Add pixel_y new rows to each of the outputs.
+    map(lambda x: x.extend([[] for _ in xrange(pixel_y)]), outputs)
+    for orig_pixel in row:
+      # Processing a pixel of the original image.
+      # Which set of mask sets to use?
+      masks_to_use = (black_masks, white_masks)[orig_pixel]
+      # Pick one mask set (one for each output image).
+      masks = random.choice(masks_to_use)
+      assert n == len(masks)
+      assert n == len(outputs)
+      for output, mask in zip(outputs, masks):  # TODO(randomization!)
+        assert pixel_y == len(mask)
+        for y, mask_row in enumerate(mask):
+          assert pixel_x == len(mask_row)
+          # Fill the last rows.
+          output[-pixel_y+y].extend(mask_row)
+  return outputs
---
+++ encoder_test.py
@@ -0,0 +1,34 @@
+import unittest2 as unittest
+import encoder as e
+
+import random
+
+import vicry
+
+class ApplyTest(unittest.TestCase):
+  IMG0 = [[0]]
+  IMG1 = [[1]]
+  IMG2 = [[0,1]]
+  IMG3 = [[0], [1]]
+  IMG4 = [[1,1],[0,1]]
+
+  def testGetParams(self):
+    self.assertEqual((2,2,2), e.get_params(vicry.M[(2,2)]['a']))
+    self.assertEqual((3,2,2), e.get_params(vicry.M[(3,3)]['a']))
+
+  def testApplyMasks_1_1(self):
+    o = e.apply_masks(self.IMG0, vicry.M[(1,1)]['a'], random.Random(3))
+    self.assertEqual(list(o), [[[0]]])
+
+    o = e.apply_masks(self.IMG4, vicry.M[(1,1)]['a'], random.Random(3))
+    self.assertEqual(list(o), [[[1, 1], [0, 1]]])
+
+    o = e.apply_masks(self.IMG0, vicry.M[(1,1)]['t12'], random.Random(3))
+    self.assertEqual(list(o), [[[0,0]]])
+
+    o = e.apply_masks(self.IMG4, vicry.M[(1,1)]['t12'], random.Random(3))
+    self.assertEqual(list(o), [[[1,1,1,1], [0,0,1,1]]])
+
+  def testApplyMasks_2_2(self):
+    o = e.apply_masks(self.IMG0, vicry.M[(2,2)]['a'], random.Random(8))
+    self.assertEqual(list(o),  [[[0, 1], [1, 0]], [[0, 1], [1, 0]]])
